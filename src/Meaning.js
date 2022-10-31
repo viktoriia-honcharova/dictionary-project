0b38456cfd892ec57b87509431ba0219af593458
Viktoriia Honcharova <vika.honcharova.619853@gmail.com>
2022-10-31 17:33:44 +0200

    Added the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,25 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        {props.meaning.definitions[0].definition}
-        <br />
-        <em>{props.meaning.definitions[0].example}</em>
-      </p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition: </strong>
+              {definition.definition}
+              <br />
+              <strong>Example: </strong>
+              <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
