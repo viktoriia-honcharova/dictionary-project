d558305c7fcbf497ccaf1dc599162362f34a073e
Viktoriia Honcharova <vika.honcharova.619853@gmail.com>
2022-10-31 20:13:15 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -14,6 +14,32 @@ function App() {
         <main>
           <Dictionary />
         </main>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.instagram.com/v.honcharova/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Viktoriia Honcharova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/viktoriia-honcharova/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://dazzling-basbousa-cdb610.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
